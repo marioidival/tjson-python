a95dd8748e5d939e64900f5d9ee548f50e650444
Mario Idival <marioidival@gmail.com>
2016-11-05 04:21:30 -0300

    initial package

---
+++ docs/usage.rst
@@ -0,0 +1,7 @@
+=====
+Usage
+=====
+
+To use TJSON Python in a project::
+
+    import tjson_python
---
+++ tests/test_tjson_python.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+test_tjson_python
+----------------------------------
+
+Tests for `tjson_python` module.
+"""
+
+
+import sys
+import unittest
+
+from tjson_python import tjson_python
+
+
+
+class TestTjson_python(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_000_something(self):
+        pass
